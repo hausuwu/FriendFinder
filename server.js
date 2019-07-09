d44cb715717b413d7d3baf75b765c660efcbcbb9
hausu <kennedymj2@gmail.com>
2019-07-08 21:04:43 -0500

    initialized files and wrote html pages, setup server and html routes

---
+++ server.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+// ==============================================================================
+
+var express = require("express");
+var path = require("path");
+
+// EXPRESS CONFIGURATION
+// ==============================================================================
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// LISTENER
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT:" + PORT);
+  });
